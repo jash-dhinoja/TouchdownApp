2ce7c85dc8643b30f2552e5e4589c46cf964004f
Jash Dhinoja <jash.dhinoja@gmail.com>
2023-03-13 19:13:45 -0400

    Initial Commit

---
+++ TouchdownApp/TouchdownAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownAppApp.swift
+//  TouchdownApp
+//
+//  Created by Jash Dhinoja on 13/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
